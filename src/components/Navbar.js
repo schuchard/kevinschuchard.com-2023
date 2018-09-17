a8aec7f2e6d25bfdcc791164efd9183c15921ffc
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-09-16 21:42:37 -0600

    change github link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => (
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/schuchard"
           target="_blank"
           rel="noopener noreferrer"
         >
