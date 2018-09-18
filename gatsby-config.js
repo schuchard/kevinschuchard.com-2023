ae7e4cfee2ee0bcb3b028e6d253a5ae261f3f798
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-09-17 20:44:45 -0600

    uptdate site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Kevin Schuchard',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Kevin Schuchard" />
     <Navbar />
     <div>{children}</div>
   </div>
