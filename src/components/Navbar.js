9174e1ae5fc017810386aa22175b6891f83f0f93
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-09-24 21:42:11 -0600

    toggle navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,48 +2,61 @@ import React from 'react';
 import { Link } from 'gatsby';
 import github from '../img/github-icon.svg';
 
-const Navbar = () => (
-  <nav class="navbar is-transparent">
-    <div class="navbar-brand">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">Kevin Schuchard</figure>
-        </Link>
-      </div>
-      <div class="navbar-burger burger" data-target="navBar">
-        <span />
-        <span />
-        <span />
-      </div>
-    </div>
+export default class NavBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isActive: false,
+    };
+  }
 
-    <div id="navBar" class="navbar-menu">
-      <div class="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-      </div>
-    </div>
+  toggleActive = () => {
+    this.setState((state) => ({ isActive: !state.isActive }));
+  };
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="field is-grouped">
-          <p class="control">
-            <a
-              className="navbar-item"
-              href="https://github.com/schuchard"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon">
-                <img src={github} alt="Github" />
-              </span>
-            </a>
-          </p>
+  render() {
+    return (
+      <nav className="navbar is-transparent">
+        <div className="navbar-brand">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              <figure className="image">Kevin Schuchard</figure>
+            </Link>
+          </div>
+          <div className="navbar-burger burger" data-target="navBar" onClick={this.toggleActive}>
+            <span />
+            <span />
+            <span />
+          </div>
         </div>
-      </div>
-    </div>
-  </nav>
-);
 
-export default Navbar;
+        <div id="navBar" className={'navbar-menu' + (this.state.isActive ? ' is-active' : '')}>
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/about">
+              About
+            </Link>
+          </div>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="field is-grouped">
+              <p className="control">
+                <a
+                  className="navbar-item"
+                  href="https://github.com/schuchard"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <span className="icon">
+                    <img src={github} alt="Github" />
+                  </span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
