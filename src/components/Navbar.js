847b27c291935e364a5d92bfef32dd8e41b9280b
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-09-24 20:20:50 -0600

    add navbar elements (no js)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,32 +3,44 @@ import { Link } from 'gatsby';
 import github from '../img/github-icon.svg';
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
+  <nav class="navbar is-transparent">
+    <div class="navbar-brand">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">Kevin Schuchard</figure>
         </Link>
       </div>
-      <div className="navbar-start">
+      <div class="navbar-burger burger" data-target="navBar">
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+
+    <div id="navBar" class="navbar-menu">
+      <div class="navbar-start">
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/">
-          Blog
-        </Link>
       </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/schuchard"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="field is-grouped">
+          <p class="control">
+            <a
+              className="navbar-item"
+              href="https://github.com/schuchard"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="icon">
+                <img src={github} alt="Github" />
+              </span>
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   </nav>
