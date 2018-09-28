de407bfd76bb5418cd1737f526d8a4bc91fa0f5a
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-09-27 22:09:36 -0600

    fonts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,10 +6,13 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Kevin Schuchard" />
+    <Helmet>
+      <title>Kevin Schuchard</title>
+      <link rel="preload" as="font" crossorigin href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto:900" rel="stylesheet" />
+    </Helmet>
     <Navbar />
     <div>{children}</div>
   </div>
-)
+);
 
 export default TemplateWrapper
