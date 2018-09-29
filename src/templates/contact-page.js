d071321d682039ebad0e905a3eade8ea9f533cdf
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-09-28 22:41:03 -0600

    form wired, no validation

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -9,11 +9,51 @@ import user from '../img/user.svg';
 class ContactPageTemplate extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      userName: '',
+      userEmail: '',
+      userMessage: '',
+      touched: {
+        userEmail: false,
+        userName: false,
+        userMessage: false,
+      },
+    };
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  validate(userEmail, userName, userMessage) {
+    return {
+      userEmail: userEmail.length === 0,
+      userName: userName.length === 0,
+      userMessage: userMessage.length === 0,
+    };
+  }
+
+  handleBlur = (field) => (evt) => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+    });
+  };
+
+  canBeSubmitted() {
+    const errors = this.validate(this.state.userEmail, this.state.userName, this.state.userMessage);
+    const isDisabled = !Object.keys(errors).some((x) => errors[x]);
+    return { ...errors, ...isDisabled };
   }
 
   render() {
     const PageContent = this.props.contactComponent || Content;
+    // const { errors } = this.canBeSubmitted();
 
     return (
       <section className="section section--gradient">
@@ -25,44 +65,62 @@ class ContactPageTemplate extends React.Component {
                   {this.props.title}
                 </h2>
                 <PageContent className="content" content={this.props.content} />
-
-                <div class="field">
-                  <label class="label">Name</label>
-                  <div class="control has-icons-right">
-                    <input class="input" type="text" placeholder="Text input" />
-                    <span class="icon is-small is-right">
-                      <img src={user} alt="name" />
-                    </span>
+                <form>
+                  <div className="field">
+                    <label className="label">Name</label>
+                    <div className="control has-icons-right">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="Name"
+                        name="userName"
+                        value={this.state.userName}
+                        onChange={this.handleInputChange}
+                      />
+                      <span className="icon is-small is-right">
+                        <img src={user} alt="name" />
+                      </span>
+                    </div>
                   </div>
-                </div>
 
-                <div class="field">
-                  <label class="label">Email</label>
-                  <div class="control has-icons-right">
-                    <input
-                      class="input is-danger"
-                      type="email"
-                      placeholder="Email input"
-                    />
-                    <span class="icon is-small is-right">
-                      <img src={mail} alt="mail" />
-                    </span>
+                  <div className="field">
+                    <label className="label">Email</label>
+                    <div className="control has-icons-right">
+                      <input
+                        className="input is-danger"
+                        type="email"
+                        placeholder="Email"
+                        name="userEmail"
+                        value={this.state.userEmail}
+                        onChange={this.handleInputChange}
+                        //   onBlur={this.handleBlur('email')}
+                      />
+                      <span className="icon is-small is-right">
+                        <img src={mail} alt="mail" />
+                      </span>
+                    </div>
+                    {/* <p className="help is-danger">This email is invalid</p> */}
                   </div>
-                  {/* <p class="help is-danger">This email is invalid</p> */}
-                </div>
 
-                <div class="field">
-                  <label class="label">Message</label>
-                  <div class="control">
-                    <textarea class="textarea" placeholder="Textarea" />
+                  <div className="field">
+                    <label className="label">Message</label>
+                    <div className="control">
+                      <textarea
+                        className="textarea"
+                        placeholder="Message"
+                        name="userMessage"
+                        value={this.state.userMessage}
+                        onChange={this.handleInputChange}
+                      />
+                    </div>
                   </div>
-                </div>
 
-                <div class="field is-grouped">
-                  <div class="control">
-                    <button class="button is-link">Submit</button>
+                  <div className="field is-grouped">
+                    <div className="control">
+                      <button className="button is-link">Submit</button>
+                    </div>
                   </div>
-                </div>
+                </form>
               </div>
             </div>
           </div>
