b5c348c3e5b45fc709eb5b656f10bdd0bf60162d
Kevin Schuchard <schuchard.kevin@gmail.com>
2018-10-05 21:43:02 -0600

    fix font loading, font-face url, preload

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,8 @@ const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet>
       <title>Kevin Schuchard</title>
-      <link rel="stylesheet" as="font" crossorigin href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto:900"/>
+      <link rel="preload" href="https://fonts.gstatic.com/s/roboto/v18/KFOlCnqEu92Fr1MmYUtfBBc4AMP6lQ.woff2" as="font" crossorigin type="font/woff2"></link>
+      <link rel="preload" href="https://fonts.gstatic.com/s/opensans/v15/mem8YaGs126MiZpBA-UFVZ0bf8pkAg.woff2" as="font" crossorigin type="font/woff2"></link>
     </Helmet>
     <Navbar />
     <div>{children}</div>
