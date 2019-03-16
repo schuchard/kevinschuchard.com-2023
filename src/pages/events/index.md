e89bb078db50ca2bbdc539123eefca62dcbd735a
kevin schuchard <schuchard.kevin@gmail.com>
2019-03-16 08:49:30 -0600

    loop over events

--- src/pages/events/index.md
+++ src/pages/events/index.md
@@ -1,13 +1,19 @@
 ---
 templateKey: 'events-page'
 path: /events
-title: Events
+title: Public Events
 image: ''
-heading: Public Events
 description: >-
   Presentations, meetups, and podcasts.
 events:
   - image: /img/coffee.png
+    alt: 'alt'
+    externalLink: 'external-link'
+    embedLink: 'embed-link'
+    title: 'title'
+    description: >
+      We sell green and roasted coffee beans
+  - image: /img/coffee2.png
     alt: 'alt'
     externalLink: 'external-link'
     embedLink: 'embed-link'
--- src/templates/events-page.js
+++ src/templates/events-page.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import Content, { HTMLContent } from '../components/Content';
 
-export const EventsPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content
+export const EventsPageTemplate = ({ title, content, contentComponent, events }) => {
+  const PageContent = contentComponent || Content;
 
   return (
     <section className="section section--gradient">
@@ -13,26 +13,28 @@ export const EventsPageTemplate = ({ title, content, contentComponent }) => {
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">{title}</h2>
+              {events.map((item) => (
+                <div key={item.image}>{JSON.stringify(item)}</div>
+              ))}
               <PageContent className="content" content={content} />
             </div>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 EventsPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
+  events: PropTypes.any,
   contentComponent: PropTypes.func,
-}
+};
 
 const EventsPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { markdownRemark: post } = data;
 
   return (
     <Layout metaDesc="Kevin Schuchard Events page">
@@ -40,16 +42,17 @@ const EventsPage = ({ data }) => {
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        events={post.frontmatter.events}
       />
     </Layout>
-  )
-}
+  );
+};
 
 EventsPage.propTypes = {
   data: PropTypes.object.isRequired,
-}
+};
 
-export default EventsPage
+export default EventsPage;
 
 export const eventsPageQuery = graphql`
   query EventsPage($id: String!) {
@@ -57,7 +60,15 @@ export const eventsPageQuery = graphql`
       html
       frontmatter {
         title
+        events {
+          title
+          description
+          image
+          alt
+          externalLink
+          embedLink
+        }
       }
     }
   }
-`
+`;
