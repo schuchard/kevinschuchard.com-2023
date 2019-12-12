3b1a17b1ea6255a480bb601ad8bc44e0844e51c0
kevin schuchard <schuchard.kevin@gmail.com>
2019-12-11 21:16:12 -0700

    remove cms, don't build as last deploy step

--- gatsby-config.js
+++ gatsby-config.js
@@ -190,12 +190,6 @@ module.exports = {
         clientsClaim: true,
       },
     },
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 };
